5c622f8a47c66e1ad92b07be9e6856eb012bcb0a
sawan5216977 <sawansingh5216977@gmail.com>
2019-03-20 14:00:08 +0530

    first commit

---
+++ app/src/main/java/com/example/demodatabase/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.demodatabase;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button registerButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        registerButton = findViewById(R.id.send_to_registration_page_button);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendUserToRegisterActivity();
+            }
+        });
+    }
+
+    private void sendUserToRegisterActivity() {
+
+        Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+        startActivity(intent);
+
+    }
+}
